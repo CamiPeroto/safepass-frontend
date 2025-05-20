24ce70185467523e009ba77645105dcadeab75f9
CamiPeroto <camilabuenoperoto@gmail.com>
2025-05-20 16:59:35 -0300

    feat: instalando o relume

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,4 +13,8 @@ const config: Config = {
   },
   plugins: [require("tailwindcss-animate")],
 }
+module.exports = {
+  content: ["./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}"],
+  presets: [require("@relume_io/relume-tailwind")],
+};
 export default config;
